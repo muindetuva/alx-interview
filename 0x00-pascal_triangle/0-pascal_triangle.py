1cd3e55026f639a3f8dfd1738881efcc207366f6
Alfred Tuva <muindetuva@gmail.com>
2022-07-18 10:43:02 +0300

    Add pascal triangle code

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+'''
+Pascal triangle function
+'''
+
+def pascal_triangle(n):
+    '''
+    Returns a list of lists representing the pascal triangle of n
+    '''
+
+    res = [[1]]
+
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+
+        res.append(row)
+
+    return res
